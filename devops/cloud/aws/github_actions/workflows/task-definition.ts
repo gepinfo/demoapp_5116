889c541dfda0864b8f0cdae9596efb743c63d507
gepinfo <gepinfo>
2025-01-16 11:36:43 +0000

    updated generated code

--- devops/cloud/aws/github_actions/workflows/task-definition.ts
+++ devops/cloud/aws/github_actions/workflows/task-definition.ts
@@ -70,7 +70,7 @@ module.exports = {
             ],
             "environmentFiles": null,
             "environment": [{"name":"MONGO_DB_URL","value":"mongodb://admin:password@127.0.0.1:27017/demoapp_5116?authSource=admin"},{"name":"MONGO_DOMAIN","value":"127.0.0.1"},{"name":"SECURITYURL","value":"http://127.0.0.1:8003"},{"name":"AUTHPROXYURL","value":"http://127.0.0.1:8001"},{"name":"ADMINURL","value":"http://127.0.0.1:8004"},{"name":"GCAMURL","value":"http://127.0.0.1:8007"},{"name":"APIGATEWAY","value":"http://127.0.0.1:8000"},{"name":"SYSTEMCREDENTIALMANAGERURL","value":"http://127.0.0.1:8005"}
-            ,{"name":"SYSTEMENTRYURL","value":"http://127.0.0.1:8024"}
+            ,{"name":"SYSTEMENTRYURL","value":"http://127.0.0.1:8025"}
             ],
             "secrets": null,
             "mountPoints": [
@@ -292,7 +292,7 @@ module.exports = {
             ],
             "environmentFiles": null,
             "environment": [{"name":"MONGO_DB_URL","value":"mongodb://admin:password@127.0.0.1:27017/demoapp_5116?authSource=admin"},{"name":"MONGO_DOMAIN","value":"127.0.0.1"},{"name":"SECURITYURL","value":"http://127.0.0.1:8003"},{"name":"AUTHPROXYURL","value":"http://127.0.0.1:8001"},{"name":"ADMINURL","value":"http://127.0.0.1:8004"},{"name":"GCAMURL","value":"http://127.0.0.1:8007"},{"name":"APIGATEWAY","value":"http://127.0.0.1:8000"}
-            ,{"name":"SYSTEMENTRYURL","value":"http://127.0.0.1:8024"}
+            ,{"name":"SYSTEMENTRYURL","value":"http://127.0.0.1:8025"}
             ],
             "mountPoints": [
                 {
@@ -373,9 +373,9 @@ module.exports = {
             "essential": true,
             "portMappings": [
                 {
-                    "containerPort": "8024",
+                    "containerPort": "8025",
                     "protocol": "tcp",
-                    "hostPort": "8024"
+                    "hostPort": "8025"
                 }
             ],
             "environmentFiles": null,
